39aa9fdd7d90ca0855789525967b17bf147f9766
duanyufei <duanyufei@bytedance.com>
2021-12-05 16:30:49 +0800

    Done extract

--- pdfparser.js
+++ pdfparser.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+const OUTPUT_TMP = false
+const HEADER_ENDING = 'Counter Party'
+// const TARGET_FILE = "./sample/bank_test.pdf"
+const TARGET_FILE = "./sample/cmb.pdf"
+
 exports.pdfParser = function () {
     console.log('start')
     const fs = require('fs'),
@@ -10,27 +15,34 @@ exports.pdfParser = function () {
     pdfParser.on("pdfParser_dataReady", pdfData => {
         var result = JSON.stringify(pdfData, null, 2);
         result = decodeURIComponent(result)
-        fs.writeFile('./sample/tmp.json', result.toString(), callback => {
-            result = JSON.parse(result)
-            result = extract(result)
-            console.log(result);
-            fs.writeFile('./sample/result.json', result.toString(), callback => {
+        if (OUTPUT_TMP) {
+            fs.writeFile('./sample/tmp.json', result.toString(), callback => {
             })
+        }
+        result = extract(pdfData)
+        console.log(result);
+        fs.writeFile('./sample/result.json', result.join('\n'), callback => {
         })
     });
-    pdfParser.loadPDF("./sample/bank_test.pdf");
-
-    console.log('done')
+    pdfParser.loadPDF(TARGET_FILE)
 }
 
 function extract(raw) {
-    let pages = raw['pages']
-    var texts = []
-    for (page in pages) {
-        for (text in page) {
-            let t = text['R']['T']
-            texts.add(t)
-        }
-    }
+    let afterCounterParty = false;
+    let pages = raw['Pages']
+    const texts = []
+    pages.forEach(e => {
+        // console.log(e['Texts'])
+        e['Texts'].forEach(i => {
+            let target = i['R'][0]['T']
+            let result = decodeURIComponent(target)
+            if (afterCounterParty) {
+                texts.push(result)
+                console.log(result)
+            } else if (result == HEADER_ENDING) {
+                afterCounterParty = true // 表示 Header 部分结束, 明细部分开始
+            }
+        })
+    })
     return texts
 }
